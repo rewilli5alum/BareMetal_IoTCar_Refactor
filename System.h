3be4ea894ea5b29a0465bc4105f9664de6382f5e
win_REW <rewilli5@alumni.ncsu.edu>
2025-02-24 16:35:57 -0500

    created System.c; updated main

---
+++ System.h
@@ -0,0 +1,21 @@
+//******************************************************************************
+//   Name: System.h
+//   Author: Rachel E. Williams
+//   Date Created: February 24, 2025
+//   Last Updated: 2/24/2025
+//   Description: This is the header file for System.c, containing 
+//                function declarations and relevant macros 
+//   Built with IAR Embedded Workbench IDE - MSP430 V8.10.3 
+//******************************************************************************  
+
+/* Macros */ 
+
+/* Function declarations */ 
+//------------------------------------------------------------------------------
+//   Name: xxxx()
+//   Description: xxxx 
+//   Global variables: N/A
+//   Input parameters: N/A
+//   Return: N/A
+//------------------------------------------------------------------------------
+void System_InitAll(void); 
--- main.c
+++ main.c
@@ -2,13 +2,15 @@
 //   Name: main.c
 //   Author: Rachel E. Williams
 //   Date Created: February 22, 2025
-//   Last Updated: 2/22/2025
+//   Last Updated: 2/24/2025
 //   Description: This file contains the main routine to run the entire project
 //                using while(1) "Super Loop" architecture
 //   Built with IAR Embedded Workbench IDE - MSP430 V8.10.3 
 //******************************************************************************  
 
 /* #includes */ 
+#include "msp430.h" 
+#include "System.h"
 
 /* Global Variables */ 
 
@@ -16,6 +18,13 @@
 
 
 void main(void){
+  // system-wide init 
+  System_InitAll();   
   
+  //GPIO-specific init 
+  //---->LCD.c 
+  //---->IoT.c '
+  //---->Switches.c 
+  //---->Motors.c 
 
 }
